0a138f50b90a5df38aed7d3fec998ede6b7d3215
Tashriq Hendricks <tashriq93@gmail.com>
2026-01-03 15:38:03 +0200

    Remove Gradle wrapper jar from repo

---
+++ app/src/main/java/com/stormyai/app/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.stormyai.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StormyAiApp()
+        }
+    }
+}
+
+@Composable
+private fun StormyAiApp() {
+    MaterialTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Text(text = "StormyAi")
+        }
+    }
+}
